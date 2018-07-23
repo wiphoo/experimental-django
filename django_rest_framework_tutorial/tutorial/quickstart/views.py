93fab29c1deb02fbf27b2833fd7fe9e4aae12c1c
Wiphoo (Terng) Methachawalit <wiphoo.m@gmail.com>
2018-07-23 23:48:55 +0700

    added token based authentication on users pages

--- django_rest_framework_tutorial/tutorial/quickstart/views.py
+++ django_rest_framework_tutorial/tutorial/quickstart/views.py
@@ -9,8 +9,15 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 class UserViewSet( viewsets.ModelViewSet ):
 	''' API endpoint that allows users to be viewed or edited. '''
+	
+	authentication_classes = (TokenAuthentication,)
+	permission_classes = (IsAuthenticated,)
+	
 	queryset = User.objects.all().order_by( '-date_joined' )
 	serializer_class = UserSerializer
 	
